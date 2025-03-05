ca6b04fcc82c601c713d31b17c8b6fd60d230a65
Alex <alex.mani@student.moringaschool.com>
2025-03-05 20:53:01 +0300

    Done

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // code your solution here
+function saturdayFun(activity) {
+    if (activity === undefined) {
+        activity = "roller-skate";
+    }
+    return `This Saturday, I want to ${activity}!`;
+}
+
+const mondayWork = function(activity) {
+    if (activity === undefined) {
+        activity = "go to the office";
+    }
+    return `This Monday, I will ${activity}.`;
+};
+
+function wrapAdjective(wrapper = "*") {
+    return function(adjective = "special") {
+        return `You are ${wrapper}${adjective}${wrapper}!`;
+    }
+} 
